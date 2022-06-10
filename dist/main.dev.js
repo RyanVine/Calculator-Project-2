06ed473e5e887754660461ae4048c5bf0fbcff27
Ryan Vine <R.vine66@gmail.com>
2022-06-10 11:58:36 +0100

    Added buttins in JS

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var button = document.getElementsByClassName("calculator__button");
+var numbers = document.querySelectorAll(".calculator__number");
+var operators = document.querySelectorAll(".calculator__operator");
+var display = document.querySelector(".calculator__display");
+var clear = document.querySelector(".calculator__clear");
+var plusMinus = document.querySelector(".calculator__plus-or-minus");
+var percent = document.querySelector(".calculator__percent");
+var decimal = document.querySelector(".calculator__button-decimal");
+var equals = document.querySelector("#equals");
---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByClassName("calculator__button");
+const numbers = document.querySelectorAll(".calculator__number");
+const operators = document.querySelectorAll(".calculator__operator");
+const display = document.querySelector(".calculator__display");
+
+const clear = document.querySelector(".calculator__clear");
+const plusMinus = document.querySelector(".calculator__plus-or-minus");
+const percent = document.querySelector(".calculator__percent");
+const decimal = document.querySelector(".calculator__button-decimal");
+const equals = document.querySelector("#equals");
